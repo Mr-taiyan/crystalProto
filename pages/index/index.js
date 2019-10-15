8f114a9bd722400d94b1cc3cee5e002b5ea0d8ca
chen <cty9388@hotmail.com>
2019-10-15 16:42:29 +0800

    add gallery

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,9 +16,24 @@ Page({
     ]
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: "../logs/logs"
+  bindViewTap: function(e) {
+    let prefix =
+      "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master";
+    let currentImg = prefix + e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: currentImg,
+      urls: [
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-1.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-2.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-3.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-4.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-1.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-2.png",
+        "https://raw.githubusercontent.com/Mr-taiyan/crystalProto/master/images/image1-3.png"
+      ],
+      success: function(e) {
+        console.log(e);
+      }
     });
   },
   onLoad: function() {},
