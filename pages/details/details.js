fc540f812db77e5560f1c9c33607e940feb19038
chen <cty9388@hotmail.com>
2019-10-17 16:38:37 +0800

    finish navigation bar

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,66 +1,52 @@
 // pages/details/details.js
 Page({
-
   /**
    * Page initial data
    */
   data: {
-
+    navTitle: "cty's test",
+    navBackground: "gray",
+    navColor: "",
+    navLoading: false
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
