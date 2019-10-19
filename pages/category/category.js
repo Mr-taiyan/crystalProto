b0dde10fa1698f6b47d8df010579b86b5f9d962c
chen <cty9388@hotmail.com>
2019-10-19 19:51:28 +0800

    simulate fake product data

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,66 @@
 // pages/category/category.js
 Page({
-
   /**
    * Page initial data
    */
   data: {
-
+    productInfo: {}
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.setData({
+      productInfo: [
+        {
+          typeName: "水晶茶具",
+          products: []
+        },
+        {
+          typeName: "水晶家私",
+          products: []
+        },
+        {
+          typeName: "水晶首饰",
+          products: []
+        }
+      ]
+    });
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
